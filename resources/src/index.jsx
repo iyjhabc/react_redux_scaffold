43f0a5eee4bc0f20325c8e6b3e2e343aa27013fb
apple <apple@rexyan-MC1.local>
2016-06-10 11:37:55 +0800

    use browser api

--- resources/src/index.jsx
+++ resources/src/index.jsx
@@ -5,16 +5,18 @@ import { Provider ,connect } from 'react-redux';
 import App from 'containers/App/index.jsx'
 import IndexPage from 'containers/IndexPage/index.jsx'
 import AboutPage from 'containers/AboutPage/index.jsx'
+import NotFoundPage from 'containers/NotFoundPage/index.jsx'
 import {myApp} from 'reducers/app/appReducers.js'
-import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory, browserHistory } from 'react-router'
 
 let store = createStore(myApp);
 
 const component = (
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
         <Route path="/index" component={IndexPage}/>
         <Route path="/about" component={AboutPage}/>
+        <Route path="/*" component={NotFoundPage}/>
     </Route>
     
   </Router>
